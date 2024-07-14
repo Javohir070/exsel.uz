00c17334243b094df7fb63cc138f63efaac45504
Javohir070 <qayumovjavohir24@gmail.com>
2024-07-14 19:23:15 +0500

    baza yuklash

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -4,42 +4,60 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Response;
 
 class ExportController extends Controller
 {
     public function export()
     {
-        $filename = "data_" . date('Y-m-d') . ".csv";
-        $handle = fopen('php://output', 'w');
-        $columns = [];
+        $servername = "localhost";
+        $username = "suhrob";
+        $password = "123P@ssw0rd";
+        $dbname = "exsel";
+
+        // Bazaga ulanish
+        $conn = new \mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
         // Jadval nomi
-        $table = "tashkilots";
+        $table = "users";
 
         // Ma'lumotlarni olish
-        $users = DB::table($table)->get();
+        $sql = "SELECT * FROM $table";
+        $result = $conn->query($sql);
 
-        if ($users->isEmpty()) {
-            return response()->json(['message' => 'No data found'], 404);
+        if ($result->num_rows > 0) {
+            $delimiter = ",";
+            $filename = "data_" . date('Y-m-d') . ".csv";
+
+            // Faylni ochish
+            $f = fopen('php://memory', 'w');
+
+            // Sarlavhalarni yozish
+            $fields = $result->fetch_fields();
+            $header = array();
+            foreach ($fields as $field) {
+                $header[] = $field->name;
+            }
+            fputcsv($f, $header, $delimiter);
+
+            // Ma'lumotlarni yozish
+            while ($row = $result->fetch_assoc()) {
+                $line = array();
+                foreach ($header as $col) {
+                    $line[] = $row[$col];
+                }
+                fputcsv($f, $line, $delimiter);
+            }
+
+            // Faylni ko'chirib olish
+            fseek($f, 0);
+            header('Content-Type: text/csv');
+            header('Content-Disposition: attachment; filename="' . $filename . '";');
+            fpassthru($f);
         }
-
-        // Sarlavhalarni yozish
-        $columns = array_keys((array)$users->first());
-        fputcsv($handle, $columns);
-
-        // Ma'lumotlarni yozish
-        foreach ($users as $row) {
-            fputcsv($handle, (array)$row);
-        }
-
-        // Faylni ko'chirib olish uchun tayyorlash
-        fclose($handle);
-        $headers = [
-            'Content-Type' => 'text/csv',
-            'Content-Disposition' => "attachment; filename=\"$filename\"",
-        ];
-
-        return Response::make('', 200, $headers);
+        exit;
     }
 }
