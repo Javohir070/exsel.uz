42a607ac6c043c1be69301391ff00fce83cf33ac
Javohir070 <qayumovjavohir24@gmail.com>
2024-07-18 16:19:22 +0500

    lovizim buldi

--- resources/views/admin/xodimlar/create.blade.php
+++ resources/views/admin/xodimlar/create.blade.php
@@ -112,6 +112,29 @@
                         <option value="Direktor">Direktor</option>
                         <option value="Buxgalter">Buxgalter</option>
                         <option value="Ilmiy kotib">Ilmiy kotib</option> 
+                        <option value="Ilmiy ishlar bo‘yicha direktor o‘rinbosari">Ilmiy ishlar bo‘yicha direktor o‘rinbosari</option> 
+                        <option value="Ilmiy-tadqiqot laboratoriyasi (bo‘lim) mudiri">Ilmiy-tadqiqot laboratoriyasi (bo‘lim) mudiri</option> 
+                        <option value="Umumiy masalalar bo‘yicha direktor o‘rinbosari">Umumiy masalalar bo‘yicha direktor o‘rinbosari</option> 
+                        <option value="Bosh buxgalter">Bosh buxgalter</option> 
+                        <option value="Moliya-iqtisod bo‘limi boshlig‘i">Moliya-iqtisod bo‘limi boshlig‘i</option> 
+                        <option value="Bosh muhandis">Bosh muhandis</option> 
+                        <option value="Bosh energetik">Bosh energetik</option> 
+                        <option value="Bosh mexanik">Bosh mexanik</option> 
+                        <option value="Boshqa mutaxassis">Boshqa mutaxassis</option> 
+                        <option value="Bo‘lim boshlig‘i">Bo‘lim boshlig‘i</option> 
+                        <option value="Yetakchi muhandis">Yetakchi muhandis</option> 
+                        <option value="Bosh ilmiy xodim">Bosh ilmiy xodim</option> 
+                        <option value="Yetakchi ilmiy xodim">Yetakchi ilmiy xodim</option> 
+                        <option value="Katta ilmiy xodim">Katta ilmiy xodim</option> 
+                        <option value="Kichik ilmiy xodim">Kichik ilmiy xodim</option> 
+                        <option value="Haydovchi">Haydovchi</option> 
+                        <option value="Hovli supuruvchi">Hovli supuruvchi</option> 
+                        <option value="Farrosh">Farrosh</option> 
+                        <option value="Bog‘bon">Bog‘bon</option> 
+                        <option value="Mutaxassis">Mutaxassis</option> 
+                        <option value="Ishlab chiqarish bo‘yicha direktor o‘rinbosari">Ishlab chiqarish bo‘yicha direktor o‘rinbosari</option> 
+                        <option value="Boshqa">Boshqa</option> 
+
 
                     </select><br>
                 </div>
