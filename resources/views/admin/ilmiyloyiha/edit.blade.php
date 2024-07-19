740b62fd19785a0e179a815aa3db8430d9717b73
Javohir070 <qayumovjavohir24@gmail.com>
2024-07-19 18:09:33 +0500

    summa o'zgartirish

--- resources/views/admin/ilmiyloyiha/edit.blade.php
+++ resources/views/admin/ilmiyloyiha/edit.blade.php
@@ -179,7 +179,7 @@
                     <label class="flex flex-col sm:flex-row"> <span
                             class="mt-1 mr-1 sm:mt-0 text-xs text-red-600">*</span> Tuzilgan shartnoma summasi (ming so‘mda) 
                     </label>
-                    <input type="text" name="sum" value="{{ $ilmiyloyiha->sum }}" readonly class="input w-full border mt-2" required="">
+                    <input type="text" name="sum" value="{{ $ilmiyloyiha->sum }}"  class="input w-full border mt-2" required="">
                 </div>
 
 
