baf15a9c9c57d55c4b3899e9ba6a6519c9b6e545
Javohir070 <qayumovjavohir24@gmail.com>
2024-07-26 10:35:23 +0500

    ilmiy unvon maovjau

--- resources/views/admin/xodimlar/create.blade.php
+++ resources/views/admin/xodimlar/create.blade.php
@@ -214,7 +214,7 @@
 
                         <option value="Dotsent">Dotsent</option>
                         <option value="Katta ilmiy xodim">Katta ilmiy xodim</option>
-                        <option value="Kichik ilmiy xodim">Kichik ilmiy xodim</option>
+                        <option value="Akademik">Akademik</option>
                         <option value="yoq">yoq</option>
 
                     </select><br>
--- resources/views/admin/xodimlar/edit.blade.php
+++ resources/views/admin/xodimlar/edit.blade.php
@@ -228,7 +228,7 @@
 
                         <option value="Katta ilmiy xodim" {{ $xodimlar->ilmiy_unvoni == "Katta ilmiy xodim" ? "selected" : ""}} >Katta ilmiy xodim</option>
 
-                        <option value="Kichik ilmiy xodim" {{ $xodimlar->ilmiy_unvoni == "Kichik ilmiy xodim" ? "selected" : ""}} >Kichik ilmiy xodim</option>
+                        <option value="Akademik" {{ $xodimlar->ilmiy_unvoni == "Akademik" ? "selected" : ""}} >Akademik</option>
                         
                         <option value="yoq" {{ $xodimlar->ilmiy_unvoni == "yoq" ? "selected" : ""}}>yoq</option>
                     </select>
