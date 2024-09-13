a20989fd7a1c0b09c59b6d832c180ba55dd8fc6e
Javohir070 <qayumovjavohir24@gmail.com>
2024-09-13 10:17:06 +0500

    tashkilot yuklash

--- resources/views/admin/tashkilot/tashkilotlar.blade.php
+++ resources/views/admin/tashkilot/tashkilotlar.blade.php
@@ -19,14 +19,19 @@
             </div>
             <a class="notification sm:hidden" href=""> <i data-feather="search" class="notification__icon"></i> </a>
         </div>
+
         <div>
             @role('super-admin')
-                <div>
-                    <!-- <a href="{{ route("tashqoshish.create") }}" class="button w-24 bg-theme-1 text-white">
-                        Qo'shish
-                    </a> -->
-                    <a href="{{ route('exportashkilot') }}" class="button box flex items-center text-gray-700"> <i data-feather="file-text" class="hidden sm:block w-4 h-4 mr-2"></i> Export to Excel </a>
-                </div>
+            <div>
+                <!-- <a href="{{ route("tashqoshish.create") }}" class="button w-24 bg-theme-1 text-white">
+                    Qo'shish
+                </a> -->
+                <a href="{{ route('exportashkilot') }}" class="button box flex items-center text-gray-700"> <i data-feather="file-text" class="hidden sm:block w-4 h-4 mr-2"></i> Export to Excel </a>
+            </div>
+            <a href="javascript:;" data-target="#science-paper-create-modal" data-toggle="modal" 
+                        class="button w-24 ml-3 bg-theme-1 text-white">
+                        Excel yuklash
+                    </a>
             @endrole
         
         </div>
@@ -117,5 +122,60 @@
 
 
    
+</div>
+
+
+<div class="modal" id="science-paper-create-modal">
+    <div class="modal__content modal__content--xl">
+        <div class="p-5">
+
+            <div class="intro-y col-span-12 flex flex-wrap sm:flex-no-wrap items-center mt-2">
+                <div class="w-full mt-3 sm:mt-0 sm:ml-auto md:ml-0">
+                    <form id="science-paper-create-form" method="POST" action="{{ route('import') }}"
+                        class="validate-form" enctype="multipart/form-data" novalidate="novalidate">
+                        @csrf
+                        <div class="grid grid-cols-12 gap-2">
+
+                            <div class="w-full col-span-12">
+
+                                <label class="flex flex-col sm:flex-row"> <span
+                                        class="mt-1 mr-1 sm:mt-0 text-xs text-red-600">*</span> Excle yuklash uchun shu shablonday bo'lishi shart yoki xato berdi.
+                                </label><br>
+                                <a  href="/admin/shablon-xodim.xlsx" form="science-paper-create-form"
+                                    class="input w-full mt-2 button w-24 bg-theme-1 text-white">
+                                    Shablon yuklab olish
+                                </a>
+
+                            </div><br>
+                            <div class="w-full col-span-12">
+                            
+
+                                <label class="flex flex-col sm:flex-row"> <span
+                                        class="mt-1 mr-1 sm:mt-0 text-xs text-red-600">*</span> Excel yuklash
+                                </label>
+                                <input type="file" name="file" style="padding-left: 0" class="input w-full mt-2"
+                                    required="">
+
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="px-5 pb-5 text-center">
+            
+          
+            <button type="button" data-dismiss="modal" class="button delete-cancel w-32 border text-gray-700 mr-1">
+                Bekor qilish
+            </button>
+            <button type="submit" form="science-paper-create-form"
+                class="update-confirm button w-24 bg-theme-1 text-white">
+                Qo'shish
+            </button>
+        </div>
+    </div>
 </div>
 @endsection
