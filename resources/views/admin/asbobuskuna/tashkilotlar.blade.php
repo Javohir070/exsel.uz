35ebcab1f8636ac73455a9259a922b9c17de7cbd
Javohir070 <qayumovjavohir77@gmail.com>
2025-12-08 22:02:40 +0500

    asbob uskuni impor qilish

--- resources/views/admin/asbobuskuna/tashkilotlar.blade.php
+++ resources/views/admin/asbobuskuna/tashkilotlar.blade.php
@@ -43,12 +43,10 @@
             </div>
 
             <div>
-                @can('super-admin')
                     <a href="javascript:;" data-target="#science-paper-create-modal" data-toggle="modal"
                         class="button w-24 ml-3 bg-theme-1 text-white">
                         Import
                     </a>
-                @endcan
                 <a href="{{ route('export.asbobuskunalar') }}" class="button w-24 ml-3 bg-theme-1 text-white">
                     Export
                 </a>
