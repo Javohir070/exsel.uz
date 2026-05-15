c9695bebcc6385b607cf374a27ddaaa5770a666d
Javohir070 <qayumovjavohir24@gmail.com>
2026-05-15 18:24:42 +0500

    tashkilot userlar  bu ham bo'ldi

--- app/Http/Controllers/TashkilotUserlarController.php
+++ app/Http/Controllers/TashkilotUserlarController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tashkilot;
+use Spatie\Permission\Models\Role;
 
 class TashkilotUserlarController extends Controller
 {
     public function index(Tashkilot $tashkilot)
     {
         $users = $tashkilot->user()->paginate(15);
+        $roles = Role::all();
+        $tashkilots = Tashkilot::all();
 
-        return view('role-permission.user.index',['users'=>$users]);
+        return view('role-permission.user.index',['users'=>$users, 'roles'=>$roles, 'tashkilots'=>$tashkilots]);
     }
 }
